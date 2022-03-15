52c38538f6b1b3d80bcd931fdda2bcaf7b54a1fa
abirhasan33 <abir03512@gmail.com>
2022-03-15 17:12:32 +0600

    new commit

--- js/countres.js
+++ js/countres.js
@@ -5,21 +5,41 @@ const lodeCounrise = () => {
 }
 
 const displayCountries = countrise => {
-    console.log(countrise);
+    console.log(countrise[0]);
     const allCountriseHtml = countrise.map(contry => getCountryHtml(contry));
     // console.log(allCountriseHtml[0]);
     const container = document.getElementById('countrise');
     container.innerHTML = allCountriseHtml.join(' ');
 }
 
-const getCountryHtml = country => {
+const getCountryHtml = ({name, flags, area, region}) => {
     return `
     <div class="country">
-      <h2>${country.name.common}</h2>
-      <h2>Id: ${country.area}</h2>
-      <img src="${country.flags.png}">
+      <h2>${name.common}</h2>
+      <p>Id:${area}</p>
+      <p>Conten: ${region}</p>
+      <img src="${flags.png}">
     </div>
     `
 }
+// const getCountryHtml = country => {
+//     const {name, flags, area} = country;
+//     return `
+//     <div class="country">
+//       <h2>${name.common}</h2>
+//       <h2>Id:${area}</h2>
+//       <img src="${flags.png}">
+//     </div>
+//     `
+// }
+// const getCountryHtml = country => {
+//     return `
+//     <div class="country">
+//       <h2>${country.name.common}</h2>
+//       <h2>Id:${country.area}</h2>
+//       <img src="${country.flags.png}">
+//     </div>
+//     `
+// }
 
 lodeCounrise()
